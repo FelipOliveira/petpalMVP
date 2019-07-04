a7610b595b78e3b74470b7d5bfec597ff91bc4f8
student <student@student09.local>
2019-07-04 11:11:05 -0300

    a table view esta mostrando os dados da celula.

---
+++ petpalMvp/dadosBioPetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  dadosBioPetTableViewCell.swift
+//  petpalMvp
+//
+//  Created by student on 04/07/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class dadosBioPetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
