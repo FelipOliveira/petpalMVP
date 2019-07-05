68b584a9aca738587a8df85a5aec60974b4f5c23
Caio Shimada Rabello <xcaiosr@gmail.com>
2019-07-05 12:08:56 -0300

    add local-storage example

---
+++ Local-DISKI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  armazenamentoLocal
+//
+//  Created by student on 7/5/19.
+//  Copyright © 2019 eduarda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
