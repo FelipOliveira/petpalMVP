53a7d6d33c277b14b64ce3478befc7ec6a9ec9ea
FelipOliveira <sem.ideias.na.hora@gmail.com>
2019-07-05 11:17:57 -0300

    Add files via upload

---
+++ AddPetViewController.swift
@@ -0,0 +1,49 @@
+//
+//  AddPetViewController.swift
+//  petpalMvp
+//
+//  Created by student on 05/07/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class AddPetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(AddPetViewController.dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
+        
+        title="Novo pet"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    //Calls this function when the tap is recognized.
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+
+}
